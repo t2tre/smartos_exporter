0d027d3ff566a3d8df23790a02a3fd506325fd0b
Raymond Edah <raymond.edah@secretescapes.com>
2018-04-08 23:59:19 +0100

    Completed uname collector

--- src/main/java/nu/ltd/fp/se/collector/AbstractMetricCollector.java
+++ src/main/java/nu/ltd/fp/se/collector/AbstractMetricCollector.java
@@ -1,10 +1,28 @@
 package nu.ltd.fp.se.collector;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AbstractMetricCollector {
   private MetricCollector nextCollector;
 
   public void setNextCollector(MetricCollector nextCollector) {
     this.nextCollector = nextCollector;
+  }
+
+  public String runCommand(String command) {
+    StringBuffer stdOut = new StringBuffer();
+    try {
+      Process p = Runtime.getRuntime().exec(command);
+      BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String s = null;
+      while ((s = stdInput.readLine()) != null) { stdOut.append(s); }
+    } catch (IOException io) {
+      stdOut.append(io.getMessage());
+    }
+
+    return stdOut.toString();
   }
 
   public MetricCollector getNextCollector() {
--- src/main/java/nu/ltd/fp/se/collector/UnameCollector.java
+++ src/main/java/nu/ltd/fp/se/collector/UnameCollector.java
@@ -6,11 +6,16 @@ public class UnameCollector extends AbstractMetricCollector implements MetricCol
   static final Gauge unameGauge = Gauge.build()
     .name("node_uname_info")
     .help("Labeled system information as provided by the uname system call")
-    .labelNames("domainname", "machine", "nodename", "sysname", "version")
+    .labelNames("domainname", "machine", "nodename", "release", "sysname", "version")
     .register();
 
   public UnameCollector() {
-    unameGauge.labels("fp.ltd.nu", "somehostname", "x64", "l4", "l5").set(1);
+    unameGauge.labels(this.runCommand("domainname"),
+      System.getProperty("os.arch"),
+      this.runCommand("hostname"),
+      System.getProperty("os.version"),
+      System.getProperty("os.name"),
+      this.runCommand("uname -v")).set(1);
   }
 
   public UnameCollector(MetricCollector nextCollector) {
@@ -18,8 +23,6 @@ public class UnameCollector extends AbstractMetricCollector implements MetricCol
     this.setNextCollector(nextCollector);
   }
 
-  // node_uname_info{domainname="(none)",machine="x86_64",nodename="lnx-host-01",release="4.4.0-87-generic",sysname="Linux",version="#110-Ubuntu SMP Tue Jul 18 12:55:35 UTC 2017"} 1
-  // node_uname_info{nodename="get",} 1.0
   public void collectMetric() {
     // Nothing to be done, call next collector
     if (this.getNextCollector() != null) {
